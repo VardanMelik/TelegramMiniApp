e94ba3fe0196496a1d90674974d186f6476db33c
Vardan Meliksetyan <crossgaming.co@gmail.com>
2024-10-08 01:16:27 +0400

    deploy

--- timer.js
+++ timer.js
@@ -5,4 +5,7 @@ https://www.youtube.com/watch?v=lCADUPlOWE8
 
 https://github.com/nikandr-surkov?tab=repositories&q=Make-TON-Telegram-Mini-App&type=&language=&sort=
 
+GitHub Deploy
+https://www.youtube.com/watch?v=Q9n2mLqXFpU
+
 */
